27a69aceedc398e3c6413c789fa6186a1fc4cd2c
usuari <usuari@Marta>
2018-05-15 22:43:47 +0200

    sessions i mysql fet

--- module/FuturSport/src/Controller/Plugin/AccessPlugin.php
+++ module/FuturSport/src/Controller/Plugin/AccessPlugin.php
@@ -3,10 +3,13 @@
 namespace FuturSport\Controller\Plugin;
 
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
-
+use Zend\Session\SessionManager;
 class AccessPlugin extends AbstractPlugin{
     
     public function checkAccess(){
+        $session=new SessionManager();
+        $session->start();
+        
         if($_SESSION['usuariConectat']->rol_name=='admin'){
             return 'admin';
         }
